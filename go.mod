8f2d25f23ddeebc4b83cd49efce0ed13d7817c07
Bintang Gede Hartono <bintanggg2401@gmail.com>
2023-08-16 14:27:29 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,17 @@
+module web_pkl2
+
+go 1.20
+
+require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
+	github.com/kataras/go-sessions v3.0.0+incompatible // indirect
+	github.com/klauspost/compress v1.16.3 // indirect
+	github.com/satori/go.uuid v1.2.1-0.20181028125025-b2ce2384e17b // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.48.0 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,201 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/kataras/go-sessions"
+	"golang.org/x/crypto/bcrypt"
+	// "os"
+)
+
+var db *sql.DB
+var err error
+
+type user struct {
+	ID           int
+	Username     string
+	Password     string
+	Nim          string
+	Nama         string
+	AsalInstansi string
+	MulaiPkl     time.Time
+	SelesaiPkl   time.Time
+	UploadFile   string
+	Role         int
+	Status       int
+}
+
+func connect_db() {
+	db, err = sql.Open("mysql", "root:@tcp(127.0.0.1)/web_pkl")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func routes() {
+	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/home", home)
+}
+
+
+func QueryUser(username string) user {
+	var users = user{}
+	err = db.QueryRow(`
+		SELECT id, 
+		username, 
+		password,
+		nim,
+		nama,
+		asal_instansi,
+		mulai_pkl,
+		selesai_pkl,
+		upload_file,
+		role,
+		status
+		FROM users WHERE username=?
+		`, username).
+		Scan(
+			&users.ID,
+			&users.Username,
+			&users.Password,
+			&users.Nim,
+			&users.Nama,
+			&users.AsalInstansi,
+			&users.MulaiPkl,
+			&users.SelesaiPkl,
+			&users.UploadFile,
+			&users.Role,
+			&users.Status,
+		)
+	return users
+}
+
+func checkErr(w http.ResponseWriter, r *http.Request, err error) bool {
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return false
+	}
+	return true
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.ServeFile(w, r, "views/register.html")
+		return
+	}
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	nim := r.FormValue("nim")
+	nama := r.FormValue("nama")
+	asal_instansi := r.FormValue("asal_instansi")
+	mulai_pkl := r.FormValue("mulai_pkl")
+	selesai_pkl := r.FormValue("selesai_pkl")
+	upload_file := r.FormValue("upload_file")
+	role := r.FormValue("role")
+	status := r.FormValue("status")
+
+	users := QueryUser(username)
+
+	if (user{}) == users {
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+		if len(hashedPassword) != 0 && checkErr(w, r, err) {
+			stmt, err := db.Prepare("INSERT INTO users SET username=?, password=?, nim=?, nama=?, asal_instansi=?, mulai_pkl=?, selesai_pkl=?, upload_file=?, role=?, status=?")
+			if err == nil {
+				_, err := stmt.Exec(&username, &hashedPassword, &nim, &nama, &asal_instansi, &mulai_pkl, &selesai_pkl, &upload_file, &role, &status)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+
+				http.Redirect(w, r, "/login", http.StatusSeeOther)
+				return
+			}
+		}
+	} else {
+		http.Redirect(w, r, "/register", http.StatusSeeOther)
+	}
+}
+
+func login(w http.ResponseWriter, r *http.Request){ 
+	session := sessions.Start(w, r)
+	if len(session.GetString("username")) != 0 && checkErr(w, r, err) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	if r.Method != "POST" {
+		http.ServeFile(w, r, "views/login.html")
+		return
+	}
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	users := QueryUser(username)
+
+	if (user{}) != users {
+		err := bcrypt.CompareHashAndPassword([]byte(users.Password), []byte(password))
+		if err != nil {
+			session := sessions.Start(w, r)
+			session.Set("username", users.Username)
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
+	}
+	fmt.Println("Login")
+
+	http.Redirect(w, r, "/home", http.StatusSeeOther)
+}
+
+
+
+func home(w http.ResponseWriter, r *http.Request) {
+	session := sessions.Start(w, r)
+	if len(session.GetString("username")) == 0 {
+		http.Redirect(w, r, "/login", http.StatusMovedPermanently)
+	}
+
+	var data = map[string]string{
+		"username": session.GetString("username"),
+		"message":  "Welcome to the Go !",
+	}
+	var t, err = template.ParseFiles("views/home.html")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	t.Execute(w, data)
+	
+
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	session := sessions.Start(w, r)
+	session.Clear()
+	sessions.Destroy(w, r)
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+
+func main() {
+	connect_db()
+	routes()
+
+	defer db.Close()
+
+	fmt.Println("Server running on port :8080")
+	http.ListenAndServe(":8080", nil)
+}
